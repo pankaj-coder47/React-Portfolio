ec040acad9286448a4ddd5eb05eca8e0b40c5b43
Dhanji Sharma <kdhanji183@gmail.com>
2023-07-26 22:18:19 +0530

    adding top button in your portfolio

--- portfolio/src/Components/Toool/GotoTop.js
+++ portfolio/src/Components/Toool/GotoTop.js
@@ -7,7 +7,7 @@ const GotoTop = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
   }
   const [Visible, Setvisible] =  useState(false)
-  
+
   const listenTohandle = () => {
     let heithtoHide = 250;
     const winScroll = document.body.scrollTop || document.documentElement.scrollTop
@@ -21,13 +21,14 @@ const GotoTop = () => {
   }
   useEffect(() => {
     window.addEventListener("scroll", listenTohandle)
+    return () => window.removeEventListener("scroll", listenTohandle)
   }, [])
 
   return (
     <>
    { Visible && (
     <div className='top-btn' onClick={GoHome}>
-      <FaArrowUpLong fontSize={"1.5rem"} color='black' className='btnupdown' />
+      <FaArrowUpLong fontSize={"1.5rem"} color='#fff' className='btnupdown' />
     </div>
     )}
   </>
